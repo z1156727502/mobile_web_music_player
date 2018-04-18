873d2ef30c92c8f1eb660f5461c628ca2689f6d1
dwqc <1156727502@qq.com>
2018-04-18 22:43:49 +0800

    gulp配置文件jiejuefolder字段错误及部分插件缺少参数问题；package.json修改名称、描述、关键字等

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require("gulp");
+var htmlclean = require("gulp-htmlclean")
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+var strip = require("gulp-strip-debug");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var newer = require("gulp-newer");
+var imagemin = require("gulp-imagemin");
+var connect = require("gulp-connect");
+
+var dev = 1;
+
+var folder = {
+    src: 'src/',
+    dist: 'dist/'
+}
+gulp.task('html', function () {
+    var p = gulp.src(folder.src + 'html/*')
+    .pipe(connect.reload())
+    if (!dev) {
+        p = p.pipe(htmlclean);
+    }
+    p.pipe(gulp.dest(folder.dist + 'html/'))
+})
+gulp.task('css', function () {
+    var option = [autoprefixer, cssnano];
+    var p = gulp.src(folder.src + 'css/*')
+        .pipe(less())
+        .pipe(connect.reload())
+    if (!dev) {
+        p = p.pipe(postcss(option));
+    }
+    p.pipe(gulp.dest(folder.dist + 'css/'))
+})
+gulp.task('js', function () { //公共依赖不能单独打包
+    var p = gulp.src(folder.src + 'js/*')
+    .pipe(connect.reload())
+    if (!dev) {
+        p = p.pipe(strip())
+            .pipe(uglify());
+    }
+    p.pipe(concat('main.js'))
+        .pipe(gulp.dest(folder.dist + 'js/'))
+})
+gulp.task('img', function () {
+    gulp.src(folder.src + 'img/*')
+        .pipe(newer(folder.dist + 'img/'))
+        .pipe(imagemin())
+        .pipe(connect.reload())
+        .pipe(gulp.dest(folder.dist + 'img/'))
+})
+gulp.task("watch" , function(){
+    gulp.watch(folder.src + 'html/*' , ['html']);
+    gulp.watch(folder.src + 'css/*' , ['css']);
+    gulp.watch(folder.src + 'js/*' , ['js']);
+    gulp.watch(folder.src + 'img/*' , ['img']);
+})
+gulp.task("server" , function(){
+connect.server({
+    // name : '',
+    port : '8081',
+    root : folder.dist + 'html/',
+    livereload : true
+});
+})
+gulp.task('default' , ['html' , 'css' , 'js' , 'img' , 'watch' , 'server'] , function(){
+
+})
